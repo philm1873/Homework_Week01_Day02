291a579a23c0ad211ecf164df5460cdcae8a7262
Phil Mitchell <pm1873@gmail.com>
2017-10-31 21:39:22 +0000

    string to number & month test

--- ruby_functions_practice.rb
+++ ruby_functions_practice.rb
@@ -25,3 +25,36 @@ end
 def join_string(string_1, string_2)
   return "#{string_1}#{string_2}"
 end
+
+def add_string_as_number(number_1, number_2)
+  return number_1.to_i + number_2.to_i
+end
+
+def number_to_full_month_name(number)
+  case number
+  when 1
+    return "January"
+  when 2
+    return "February"
+  when 3
+    return "March"
+  when 4
+    return "April"
+  when 5
+    return "May"
+  when 6
+    return "June"
+  when 7
+    return "July"
+  when 8
+    return "August"
+  when 9
+    return "September"
+  when 10
+    return "October"
+  when 11
+    return "November"
+  when 12
+    return "December"
+  end
+end
